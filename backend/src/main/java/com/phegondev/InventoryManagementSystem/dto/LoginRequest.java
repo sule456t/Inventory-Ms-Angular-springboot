b5cd39734262a46ef6939dc7f75ec0ad9cd2db3e
Suleiman Abdulsalaam <98602099+sule456t@users.noreply.github.com>
2026-01-28 09:58:52 +0300

    Update LoginRequest.java

--- backend/src/main/java/com/phegondev/InventoryManagementSystem/dto/LoginRequest.java
+++ backend/src/main/java/com/phegondev/InventoryManagementSystem/dto/LoginRequest.java
@@ -1,4 +1,4 @@
-package com.phegondev.InventoryManagementSystem.dto;
+package com.suleiman.InventoryManagementSystem.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;

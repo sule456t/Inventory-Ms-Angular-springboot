13c9aa63fc73d749607ee143da0f10756d58d2ac
Suleiman Abdulsalaam <98602099+sule456t@users.noreply.github.com>
2026-01-28 10:23:09 +0300

    Update InventoryManagementSystemApplicationTests.java

--- backend/src/test/java/com/suleiman/InventoryManagementSystem/InventoryManagementSystemApplicationTests.java
+++ backend/src/test/java/com/suleiman/InventoryManagementSystem/InventoryManagementSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.phegondev.InventoryManagementSystem;
+package com.suleiman.InventoryManagementSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
